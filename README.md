7e9f955cf9d1a079b6204ddeae9a7be5ef0db35d
adriananin <113365066+adriananin@users.noreply.github.com>
2023-07-19 16:36:55 +0000

    add app functionality for different location and dom update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# weather-app
+# Weather App
+
+## Overview
+
+The Weather App is a web application that provides real-time weather information for a given location. It utilizes the Visual Crossing Weather API to fetch weather data based on user input.
+
+## Features
+
+- **Current Weather:** The app displays the current weather conditions, including description, temperature, date, location, wind status, humidity, visibility, and air pressure.
+- **Forecast:** The app provides a 7-day forecast, showing the date, high and low temperatures for each day.
+- **Background Image:** The background image of the app changes based on the current weather conditions.
+- **Search Functionality:** Users can search for weather information for a specific location by entering the location name in the input field.
+- **Geolocation Conversion:** If the entered location name cannot be found, the app attempts to convert the location name into geographical coordinates using the Nominatim OpenStreetMap API.
+
+## Usage
+
+1. Launch the application in a web browser.
+2. The current weather information for the default location (Accra) will be displayed initially.
+3. To search for weather information for a different location, enter the desired location in the input field and click the search button or press Enter.
+4. The app will fetch the weather data for the entered location and display the updated information.
+5. The background image will change based on the current weather conditions.
+6. The app also provides a 7-day forecast for the entered location.
+
+## Development
+
+The Weather App is built using HTML, CSS, and JavaScript. It utilizes the Visual Crossing Weather API to fetch weather data and the Nominatim OpenStreetMap API for geolocation conversion. The code structure follows a modular approach for better organization and maintainability.
+
+## Built with
+
+- HTML
+- CSS
+- JavaScript
+- Fetch API
+
+## Continued Development
+
+- Improve error handling and error messages for API requests.
+- Implement caching mechanism to reduce API calls and improve performance.
+- Add additional weather information, such as precipitation, sunrise/sunset times, etc.
--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".locationInput");
   const btn = document.querySelector(".weather-btn");
+  const currentDescription = document.querySelector(
+    ".current-weather-description"
+  );
+  const currentTemp = document.querySelector(".current-temperature");
+  const currentDate = document.querySelector(".current-date");
+  const currentLocation = document.querySelector(".current-location-text");
+  const windStatus = document.querySelector(".wind-status");
+  const humidityStatus = document.querySelector(".humidity");
+  const visibilityStatus = document.querySelector(".visibility");
+  const pressureStatus = document.querySelector(".air-pressure");
 
   const getWeather = async () => {
-    console.log(input.value);
-    console.log("Button clicked");
+    const defaultLocation = "accra";
+
     try {
+      const location = input.value || defaultLocation;
       const r = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value}?unitGroup=metric&key=HMG46QMCWL33BVNPH78JACBKY&contentType=json`,
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=HMG46QMCWL33BVNPH78JACBKY&contentType=json`,
         { mode: "cors" }
       );
       const data = await r.json();
-      console.log(data);
+      updateDivs(data);
+      updateBg(data);
     } catch (error) {
       convertToCoordinates();
     }
@@ -34,7 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
       fetchWithCoordinates(latitude, longitude);
     } catch {
       (error) => {
-        console.log("failed");
         console.log("Error converting location:", error);
       };
     }
@@ -46,27 +57,90 @@ document.addEventListener("DOMContentLoaded", () => {
       { mode: "cors" }
     );
     const data = await r.json();
-    return console.log(data);
+    updateDivs(data);
+    updateBg(data);
   };
 
+  const updateDivs = (weatherData) => {
+    const forecastDivs = document.querySelectorAll(".forecast");
+    const celsiusSymbol = "\u00B0C";
+
+    currentDescription.innerHTML = `<p>${weatherData.currentConditions.conditions}</p>`;
+    currentTemp.innerHTML = `<p>${weatherData.currentConditions.feelslike} ${celsiusSymbol}</p>`;
+    currentDate.innerHTML = `<p>${weatherData.days[0].datetime}</p>`;
+    currentLocation.innerHTML = `<p>${weatherData.resolvedAddress}</p>`;
+
+    windStatus.innerHTML = `<p>Wind Status</p> <p>${weatherData.currentConditions.windspeed} mph</p>`;
+    humidityStatus.innerHTML = `<p>Humidity</p> <p>${weatherData.currentConditions.humidity} %</p>`;
+    visibilityStatus.innerHTML = `<p>Visibility</p> <p>${weatherData.currentConditions.visibility} miles</p>`;
+    pressureStatus.innerHTML = `<p>Air pressure</p> <p>${weatherData.currentConditions.pressure} mb</p>`;
+
+    const arr = [...forecastDivs];
+
+    arr.map((day, i) => {
+      day.innerHTML = `      
+      <p>${weatherData.days[i].datetime}</p>
+      <div class="high-low">
+        <div class="high">${weatherData.days[i].tempmax} ${celsiusSymbol}</div>
+        <div class="low">${weatherData.days[i].tempmin} ${celsiusSymbol}</div>
+      </div>`;
+    });
+  };
+
+  const updateBg = (weatherData) => {
+    const cloudyUrl = "/assets/images/conditions/cloudbg.svg";
+    const rainyUrl = "/assets/images/conditions/rainbg.svg";
+    const clearUrl = "/assets/images/conditions/Clear.svg";
+    const snowUrl = "/assets/images/conditions/snowbg.svg";
+    const sleetUrl = "/assets/images/conditions/Sleet.svg";
+    const hailUrl = "/assets/images/conditions/hail.svg";
+    const thunderStorm = "/assets/images/conditions/Thunderstorm.svg";
+
+    const conditions = weatherData.currentConditions.conditions.toLowerCase();
+    console.log(conditions);
+
+    if (conditions.includes("cloudy")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${cloudyUrl})`);
+    } else if (conditions.includes("rain")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${rainyUrl})`);
+    } else if (conditions.includes("clear")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${clearUrl})`);
+    } else if (conditions.includes("snow")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${snowUrl})`);
+    } else if (conditions.includes("sleet")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${sleetUrl})`);
+    } else if (conditions.includes("hail")) {
+      currentWeatherPane.style.setProperty("--bg-image", `url(${hailUrl})`);
+    } else if (conditions.includes("thunderstorm")) {
+      currentWeatherPane.style.setProperty(
+        "--bg-image",
+        `url(${thunderStorm})`
+      );
+    }
+  };
+
+  getWeather();
+
+  // Nav
+  const searchPane = document.querySelector(".search-pane");
+  const searchBtn = document.querySelector(".search-btn");
+  const closeBtn = document.querySelector(".close-btn");
+  const currentWeatherPane = document.querySelector(".current-weather");
+
+  const hideSearchPane = () => {
+    searchPane.style.display = "none";
+    currentWeatherPane.style.display = "grid";
+  };
+
+  const showSearchPane = () => {
+    searchPane.style.display = "grid";
+    currentWeatherPane.style.display = "none";
+  };
+
+  closeBtn.addEventListener("click", hideSearchPane);
+  searchBtn.addEventListener("click", showSearchPane);
+
   input.addEventListener("change", getWeather);
-});
-
-//working with the currentConditions property
-// days array for future days
-
-// Nav
-const searchPane = document.querySelector(".search-pane");
-const searchBtn = document.querySelector(".search-btn");
-const closeBtn = document.querySelector(".close-btn");
-const currentWeatherPane = document.querySelector(".current-weather");
-
-searchBtn.addEventListener("click", function () {
-  searchPane.style.display = "grid";
-  currentWeatherPane.style.display = "none";
-});
-
-closeBtn.addEventListener("click", function () {
-  searchPane.style.display = "none";
-  currentWeatherPane.style.display = "grid";
+  btn.addEventListener("click", getWeather);
+  btn.addEventListener("click", hideSearchPane);
 });
